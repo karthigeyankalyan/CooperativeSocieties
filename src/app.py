42fe1663c1aa1f950de937af9c8d8af289e32550
karthigeyankalyan <karthigeyankalyan@gmail.com>
2018-11-02 16:18:20 +0530

    Checking

--- src/app.py
+++ src/app.py
@@ -125,22 +125,21 @@ def installment_form(intent_id):
             eo_name = request.form['EO']
             user_id = user._id
 
-            garment = Database.find("GarmentICO", {"garment_type": garment_type})
-            wage_per_unit = None
-
-            for result_object in garment[0:1]:
-                wage_per_unit = result_object['wage_per_unit']
-
-            total_wages = int(units_required)*int(wage_per_unit)
-
-            installment = Installment(intent_id=intent_id, district=district, center=center, garment_type=garment_type,
-                                      units_required=units_required, deadline=deadline, total_wages=total_wages,
-                                      units_pm=units_pm, user_id=user_id, set_id=set_id, garment_size=garment_size,
-                                      installment_num=installment_num, uploaded_date=issue_date, eo=eo_name)
-
-            installment.save_to_mongo()
-
-            user = User.get_by_id(user_id)
+            # garment = Database.find("GarmentICO", {"district": user.district,
+            #                                        "garment_type": garment_type})
+            # wage_per_unit = None
+            #
+            # for result_object in garment[0:1]:
+            #     wage_per_unit = result_object['wage_per_unit']
+            #
+            # total_wages = int(units_required)*int(wage_per_unit)
+            #
+            # installment = Installment(intent_id=intent_id, district=district, center=center, garment_type=garment_type,
+            #                           units_required=units_required, deadline=deadline, total_wages=total_wages,
+            #                           units_pm=units_pm, user_id=user_id, set_id=set_id, garment_size=garment_size,
+            #                           installment_num=installment_num, uploaded_date=issue_date, eo=eo_name)
+            #
+            # installment.save_to_mongo()
 
             return render_template('installment_added.html', user=user)
 
