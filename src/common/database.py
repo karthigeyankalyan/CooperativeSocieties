f8e7ec6fa815446c653f62b330471164e9910c89
karthigeyankalyan <karthigeyankalyan@gmail.com>
2018-11-08 18:28:29 +0530

    Checking

--- src/common/database.py
+++ src/common/database.py
@@ -6,21 +6,21 @@ from bson.errors import InvalidId
 
 
 class Database(object):
-    # URI = "mongodb://127.0.0.1:27017"
-    # DATABASE = None
-    #
-    # @staticmethod
-    # def initialize():
-    #     client = pymongo.MongoClient(Database.URI)
-    #     Database.DATABASE = client['Coop']
-
-    URI = os.environ['MONGODB_URI']
+    URI = "mongodb://127.0.0.1:27017"
     DATABASE = None
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['heroku_1n536d42']
+        Database.DATABASE = client['Coop']
+
+    # URI = os.environ['MONGODB_URI']
+    # DATABASE = None
+    #
+    # @staticmethod
+    # def initialize():
+    #     client = pymongo.MongoClient(Database.URI)
+    #     Database.DATABASE = client['heroku_1n536d42']
 
     @staticmethod
     def insert(collection, data):
